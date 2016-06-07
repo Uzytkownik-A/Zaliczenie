d89ed76f211a44c31dbfecc63ca3ba937ee877dc
Użytkownik A <uzyt_A@onet.eu>
2016-06-07 23:45:18 +0200

    dodanie miejsca zerowego

--- Projekt.cpp
+++ Projekt.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-
+// Prototypy
+double potega(double a, unsigned int b);
+double delta(double a, double b, double c);
 
 int main() {
+	double o; unsigned int p;
+	double a; double b; double c;
+	double d;
+	double pd;
 
 	char wybor;
 
@@ -21,23 +27,45 @@ int main() {
 
 		case 'p': // potega
 
-			double a; int b;
 
 			std::cout << "Podaj podstawe: ";
-			std::cin >> a;
+			std::cin >> o;
 			std::cout << "\nPodaj potege: ";
+			std::cin >> p;
+
+			std::cout << o << "^" << p << " = " << potega(o, p) << "\n";
+
+			break;
+		case 'm': // miejsca zerowe
+
+
+			std::cout << "Podaj parametr a: ";
+			std::cin >> a;
+			std::cout << "\nPodaj parametr b: ";
 			std::cin >> b;
+			std::cout << "\nPodaj parametr c: ";
+			std::cin >> c;
 
-			std::cout << a << "^" << b << " = " << potega(a, b) << "\n";
+			d = delta(a, b, c);
+
+			if (d == 0) {
+				std::cout << "\nMiejsce zerowe: " << (double)((-b) / (2 * a)) << "\n";
+			}
+			else if (d > 0) {
+				pd = sqrt(d);
+				std::cout << "\nMiejsca zerowe: " << (double)((-b + pd) / (2 * a)) << ", " << (double)((-b - pd) / (2 * a)) << "\n";
+			}
+			else {
+				std::cout << "\nBrak rzeczywistych miejsc zerowych\n";
+			}
+
+
 
 			break;
-		case 'm': // potega
+		case 's': // pierwiastek
 
 			break;
-		case 's': // potega
-
-			break;
-		case 'k': // potega
+		case 'k': // koniec
 			return 0;
 			break;
 		}
@@ -48,10 +76,15 @@ int main() {
 }
 
 /* Funkcja przyjmuje parametry a i b -> a^b */
-double potega(double a, int b) {
+double potega(double a, unsigned int b) {
 	double zwracany = 1.0;
 	for (unsigned int i = 0; i < b; ++i) {
 		zwracany *= a;
 	}
 	return zwracany;
+}
+
+/* Funkcja zwraca delte z parametrow funkcji kwadratowej */
+double delta( double a, double b, double c ) {
+	return (b*b) - (4.0*a*c);
 }
